9e2c1ecbbcec1a9aaa49eaad93d57092da99af32
Sookhyun Kwon <rnjssh96@gmail.com>
2019-01-28 20:15:14 +0900

    Remove initialColor param

--- app/models/vsp-new-ticket.ts
+++ app/models/vsp-new-ticket.ts
@@ -1,11 +1,11 @@
-import { RawColorType } from "../config/theme";
+import { RawColorType, THEME_COLORS } from "../config/theme";
 import { TICKET_COLORS, TicketHeaderColorType } from "../config/ticket_theme";
 
 export default class VSPNewTicket {
     _theme_color: RawColorType;
 
-    constructor(initialColor: RawColorType) {
-        this._theme_color = initialColor;
+    constructor() {
+        this._theme_color = THEME_COLORS['none'];
     }
 
     setThemeColor = (colorName: TicketHeaderColorType) => {
