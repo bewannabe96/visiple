53c4089d9ed86681fc634156f1d1bc9e4d419c1a
Sookhyun Kwon <rnjssh96@gmail.com>
2019-02-05 20:38:41 +0900

    Update VSPProfile

--- src/components/vsp-profile.tsx
+++ src/components/vsp-profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, StyleProp } from 'react-native';
 
 import { THEME_COLORS, addShadowProperties } from '../types/config/theme';
 import { HORIZONTAL_UNIT } from '../types/config/size';
@@ -15,6 +15,11 @@ interface VSPProfileProps extends VSPMarginProps {
      * Casts shadow if true (by default ```true```)
      */
     castShadow?: boolean;
+
+    /**
+     * Style of the text (by default ```THEME_FONT```, ```ocean-blue```)
+     */
+    style?: StyleProp<any>;
 }
 
 /**
@@ -23,6 +28,7 @@ interface VSPProfileProps extends VSPMarginProps {
  * @property
  * - ```size```: Size of the icon (by default ```12HU```)
  * - ```castShadow```: Casts shadow if true (by default ```true```)
+ * - ```style```: Style of the text
  */
 export default class VSPProfile extends React.Component<VSPProfileProps> {
     public static defaultProps = {
@@ -39,6 +45,7 @@ export default class VSPProfile extends React.Component<VSPProfileProps> {
                 borderRadius: 0.5*this.props.size!,
                 ...(this.props.castShadow! ? addShadowProperties() : null),
                 ...decodeVSPMarginProps(this.props),
+                ...this.props.style,
             },
         });
 
