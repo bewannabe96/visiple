7e746cddab0fdff1396725d8e2e3080ccd3d1df4
rnjssh96 <46952249+rnjssh96@users.noreply.github.com>
2019-02-12 16:13:23 +0900

    Create plan.ts

---
+++ src/types/data/plan.ts
@@ -0,0 +1,76 @@
+type Place = string;
+
+type Currency = 'won' | 'dollar' | 'yen' | 'yuan';
+
+interface Cost {
+  value: number;
+	currency: Currency;
+}
+
+type Transportation =
+	| 'subway'
+	| 'bus'
+	| 'ship'
+	| 'plane'
+	| 'taxi'
+	| 'walk'
+	| 'train'
+	| 'tram'
+	| 'automobile'
+	| 'motorcycle'
+	| 'bike';
+
+type MealCategory =
+	| 'breakfast'
+	| 'brunch'
+	| 'lunch'
+	| 'dinner'
+	| 'midnightsnack'
+	| 'cafe'
+	| 'bar';
+
+interface MealPlan {
+	category: MealCategory;
+	fromPlace: Place;
+	toPlace: Place;
+}
+
+type RestCategory = 'wake' | 'retire' | 'nap';
+
+interface RestPlan {
+	category: RestCategory;
+	atPlace: Place;
+}
+
+interface MeetPlan {
+	people: string[];
+	atPlace: Place;
+}
+
+interface TravelPlan {
+	mean: Transportation;
+	fromPlace: Place;
+	toPlace: Place;
+}
+
+interface ActivityPlan {
+	title: string;
+	atPlace: Place;
+	cost: Cost;
+	information: string;
+}
+
+interface SightseeingPlan {
+	title: string;
+	atPlace: Place;
+	cost: Cost;
+	information: string;
+}
+
+type Plans = (
+	| MealPlan
+	| RestPlan
+	| MeetPlan
+	| TravelPlan
+	| ActivityPlan
+	| SightseeingPlan)[];
