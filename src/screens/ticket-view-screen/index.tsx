55d9ccaec3395aebc01f42ec0c177b1cd5f2a09f
Sookhyun Kwon <rnjssh96@gmail.com>
2019-02-21 15:44:22 +0900

    Update DEV_DATA

--- src/screens/ticket-view-screen/index.tsx
+++ src/screens/ticket-view-screen/index.tsx
@@ -11,6 +11,7 @@ import {
 } from '../../types/lib/size';
 import { THEME_COLORS } from '../../types/lib/theme';
 import { TICKET_COLORS } from '../../types/data/ticket/theme';
+import { Ticket } from '../../types/data/ticket';
 
 import VSPHeader from '../../components/vsp-header';
 import VSPContainer from '../../components/vsp-container';
@@ -21,31 +22,142 @@ import VSPText from '../../components/vsp-text';
 import PlanTimeline from './plan-timeline';
 import PackingList from './packing-list';
 
-const DEV_TICKET_COLOR = TICKET_COLORS.THEME.blue;
-const DEV_PLANS = [
-	{
-		date: new Date('2020-03-14'),
-		dayPlans: [],
+const DEV_TICKET: Ticket = {
+	id: 1,
+	title: '나혼자 여행갈꼬얌',
+	owner: '0001',
+	participants: ['0001', '0002'],
+	themeColor: TICKET_COLORS.THEME.blue,
+	period: {
+		from: new Date('2020-03-14'),
+		to: new Date('2020-03-18'),
 	},
-	{
-		date: new Date('2020-03-15'),
-		dayPlans: [],
+	packings: {
+		commonList: [
+			{
+				name: 'Item1',
+				ready: false,
+			},
+			{
+				name: 'Item2',
+				ready: true,
+			},
+		],
+		indivLists: [
+			{
+				user: '0001',
+				list: [
+					{
+						name: 'Item3',
+						ready: true,
+					},
+					{
+						name: 'Item4',
+						ready: false,
+					},
+				],
+			},
+			{
+				user: '0002',
+				list: [
+					{
+						name: 'Item5',
+						ready: false,
+					},
+					{
+						name: 'Item6',
+						ready: false,
+					},
+				],
+			},
+		],
 	},
-	{
-		date: new Date('2020-03-16'),
-		dayPlans: [],
-	},
-	{
-		date: new Date('2020-03-17'),
-		dayPlans: [],
-	},
-	{
-		date: new Date('2020-03-18'),
-		dayPlans: [],
-	},
-];
+	plans: [
+		{
+			date: new Date('2020-03-14'),
+			dayPlans: [
+				{
+					type: 'REST',
+					title: 'test',
+					time: {
+						at: new Date(2020, 2, 14, 9, 30),
+					},
+					category: 'wake',
+					atPlace: 'test',
+				},
+				{
+					type: 'MEAL',
+					title: 'meal test',
+					time: {
+						at: new Date(2020, 2, 14, 10, 30),
+						end: new Date(2020, 2, 14, 11, 30),
+					},
+					category: 'bar',
+					cost: {
+						value: 2000,
+						currency: 'KRW',
+					},
+					note: [
+						'타투컨벤션장 입장조건: 샌들, 반바지 금지타투컨벤션장 입장조건: 샌들, 반바지 금지',
+						'second',
+					],
+				},
+				{
+					type: 'TRAVEL',
+					title: 'test',
+					time: {
+						at: new Date(2020, 2, 14, 12, 0),
+						end: new Date(2020, 2, 14, 13, 30),
+					},
+					mean: 'walk',
+					move: {
+						from: 'here',
+						to: 'there',
+					},
+				},
+				{
+					type: 'ACTIVITY',
+					title: 'activity test',
+					time: {
+						at: new Date(2020, 2, 14, 13, 40),
+					},
+					atPlace: 'what',
+				},
+				{
+					type: 'SIGHTSEEING',
+					title: 'meal test',
+					time: {
+						at: new Date(2020, 2, 14, 17, 0),
+						end: new Date(2020, 2, 14, 19, 0),
+					},
+					note: [
+						'타투컨벤션장 입장조건: 샌들, 반바지 금지타투컨벤션장 입장조건: 샌들, 반바지 금지',
+					],
+				},
+			],
+		},
+		{
+			date: new Date('2020-03-15'),
+			dayPlans: [],
+		},
+		{
+			date: new Date('2020-03-16'),
+			dayPlans: [],
+		},
+		{
+			date: new Date('2020-03-17'),
+			dayPlans: [],
+		},
+		{
+			date: new Date('2020-03-18'),
+			dayPlans: [],
+		},
+	],
+};
 
-interface ITicketViewScreenProps extends IVSPScreenProps {}
+interface ITicketViewScreenProps extends IVSPScreenProps {
+	ticket: Ticket;
+}
 
 /**
  * TicketViewScreen
@@ -88,7 +200,7 @@ export default class TicketViewScreen extends React.Component<
 		const style = StyleSheet.create({
 			headerView: {
 				height: HORIZONTAL_UNIT(40),
-				backgroundColor: DEV_TICKET_COLOR,
+				backgroundColor: DEV_TICKET.themeColor,
 			},
 
 			bottomView: {
@@ -138,18 +250,21 @@ export default class TicketViewScreen extends React.Component<
 				<View style={style.bodyView}>
 					<VSPText
 						fontSize={THEME_HEADER_FONTSIZE}
-						color={DEV_TICKET_COLOR}
+						color={DEV_TICKET.themeColor}
 						fontWeight='bold'
 						marginX={VSP_EDGE_PADDING}
 						marginBottom={HORIZONTAL_UNIT(2)}
 					>
-						{'나혼자 여행갈꼬얌'}
+						{DEV_TICKET.title}
 					</VSPText>
 					<ScrollView>
-						<PackingList ticketColor={DEV_TICKET_COLOR} />
+						<PackingList
+							packings={DEV_TICKET.packings}
+							ticketColor={DEV_TICKET.themeColor}
+						/>
 						<PlanTimeline
-							plans={DEV_PLANS}
-							ticketColor={DEV_TICKET_COLOR}
+							plans={DEV_TICKET.plans}
+							ticketColor={DEV_TICKET.themeColor}
 						/>
 					</ScrollView>
 				</View>
