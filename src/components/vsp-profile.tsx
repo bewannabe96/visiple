83c0fd886f85537b37087eb100356683a10ce8cd
Sookhyun Kwon <rnjssh96@gmail.com>
2019-02-24 19:44:59 +0900

    Add overlaidTextView prop

--- src/components/vsp-profile.tsx
+++ src/components/vsp-profile.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { View, StyleSheet, StyleProp } from 'react-native';
+import { StyleSheet, StyleProp, Image, View } from 'react-native';
 
-import { THEME_COLORS, addShadowProperties } from '../types/lib/theme';
+import { addShadowProperties } from '../types/lib/theme';
 import { HORIZONTAL_UNIT } from '../types/lib/size';
 import {
 	decodeVSPMarginProps,
 	IVSPMarginProps,
 } from '../types/props/vsp-margin';
+import VSPText from './vsp-text';
 
 interface IVSPProfileProps extends IVSPMarginProps {
 	/**
 	 * Size of the icon
 	 */
 	size?: number;
+
+	/**
+	 * Overlaid text with the profile blurred
+	 */
+	overlaidText?: string;
 
 	/**
 	 * Casts shadow if true
@@ -30,28 +36,59 @@ interface IVSPProfileProps extends IVSPMarginProps {
  *
  * @property
  * - ```size```: Size of the icon (by default ```12HU```)
+ * - ```overlaidText```: Overlaid text with the profile blurred
  * - ```castShadow```: Casts shadow if true (by default ```true```)
  * - ```style```: Style of the text (by default ```THEME_FONT```, ```ocean-blue```)
  */
 export default class VSPProfile extends React.Component<IVSPProfileProps> {
 	public static defaultProps = {
 		size: HORIZONTAL_UNIT(12),
+		blur: false,
 		castShadow: true,
 	};
 
 	public render() {
 		const style = StyleSheet.create({
 			container: {
-				backgroundColor: THEME_COLORS.white,
 				height: this.props.size!,
 				width: this.props.size!,
-				borderRadius: 0.5 * this.props.size!,
 				...(this.props.castShadow! ? addShadowProperties() : null),
 				...decodeVSPMarginProps(this.props),
 				...this.props.style,
 			},
+
+			image: {
+				width: '100%',
+				height: '100%',
+				borderRadius: 0.5 * this.props.size!,
+			},
+
+			overlaidTextView: {
+				width: '100%',
+				height: '100%',
+				justifyContent: 'center',
+				alignItems: 'center',
+				position: 'absolute',
+			},
 		});
 
-		return <View style={style.container} />;
+		return (
+			<View style={style.container}>
+				<Image
+					source={require('../dev-sample-image/profile_1.png')}
+					blurRadius={
+						!!this.props.overlaidText ? this.props.size! / 3 : 0
+					}
+					style={style.image}
+				/>
+				{!!this.props.overlaidText && (
+					<View style={style.overlaidTextView}>
+						<VSPText theme='white'>
+							{this.props.overlaidText}
+						</VSPText>
+					</View>
+				)}
+			</View>
+		);
 	}
 }
