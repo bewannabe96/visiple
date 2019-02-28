810ea65af2a1e3a70e67df8273e2fd3e6917178d
Sookhyun Kwon <rnjssh96@gmail.com>
2019-02-28 15:44:18 +0900

    Delete date.ts

--- src/types/lib/date.ts
+++
@@ -1,8 +0,0 @@
-import dateFormat from 'dateformat';
-
-export function formatISODate(date: Date) {
-	// xxxx-xx-xx format
-	return dateFormat(date, 'isoDate');
-}
-
-export function generateDatesArrayFromPeriod(start: Date, end: Date) {}
