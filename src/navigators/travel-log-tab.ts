72cbd4ac6b913c6316c75da669b92068ea4b7d8e
Sookhyun Kwon <rnjssh96@gmail.com>
2019-03-11 22:52:42 +0900

    Create NewTravelLogScreen

--- src/navigators/travel-log-tab.ts
+++ src/navigators/travel-log-tab.ts
@@ -1,11 +1,16 @@
 import { createStackNavigator } from 'react-navigation';
 
 import _TravelLogScreen from '../screens/travel-log-screen';
+import _NewTravelLogScreen from '../screens/new-travel-log-screen';
 
 const TravelLogStack = createStackNavigator(
 	{
 		TravelLogScreen: {
 			screen: _TravelLogScreen,
+		},
+
+		NewTravelLogScreen: {
+			screen: _NewTravelLogScreen,
 		},
 	},
 	{
---
+++ src/screens/new-travel-log-screen/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { NavigationScreenProp } from 'react-navigation';
+
+import { IVSPScreenProps } from '../../types/props/vsp-screen';
+import {
+	THEME_FONTSIZE,
+	HORIZONTAL_UNIT,
+	VSP_TOP_PADDING,
+	VSP_EDGE_PADDING,
+} from '../../types/lib/size';
+
+import VSPHeader from '../../components/vsp-header';
+import { VSPHeaderBack } from '../../components/vsp-header-button';
+import VSPContainer from '../../components/vsp-container';
+import VSPTextInput from '../../components/vsp-textinput';
+
+interface INewTravelLogScreenProps {}
+
+/**
+ * NewTravelLogScreen
+ */
+export default class NewTravelLogScreen extends React.Component<
+	IVSPScreenProps<INewTravelLogScreenProps>
+> {
+	public static navigationOptions = ({
+		navigation,
+	}: {
+		navigation: NavigationScreenProp<any>;
+	}) => {
+		return {
+			header: (
+				<VSPHeader
+					headerTitle='새로운 로그'
+					headerLeft={VSPHeaderBack(navigation)}
+				/>
+			),
+		};
+	};
+
+	public render() {
+		const style = StyleSheet.create({
+			container: {
+				flex: 1,
+				paddingVertical: VSP_TOP_PADDING,
+				paddingHorizontal: VSP_EDGE_PADDING,
+			},
+
+			categoryView: {
+				marginVertical: HORIZONTAL_UNIT(3),
+			},
+		});
+
+		return (
+			<VSPContainer>
+				<ScrollView contentContainerStyle={style.container}>
+					<View style={style.categoryView}>
+						<VSPTextInput
+							placeholder='로그 제목을 입력해 주세요.'
+							fontSize={THEME_FONTSIZE}
+							marginTop={HORIZONTAL_UNIT()}
+						/>
+					</View>
+				</ScrollView>
+			</VSPContainer>
+		);
+	}
+}
