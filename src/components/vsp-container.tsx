d6201acd8cc490ca9a2af54192789c5ee78ae3a4
Sookhyun Kwon <rnjssh96@gmail.com>
2019-03-26 21:28:51 +0900

    Remove padding props

--- src/components/vsp-container.tsx
+++ src/components/vsp-container.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 
-import {
-	decodeVSPPaddingProps,
-	IVSPPaddingProps,
-} from '../types/props/vsp-padding';
+import { IVSPPaddingProps } from '../types/props/vsp-padding';
+import { HORIZONTAL_UNIT, VSP_EDGE_PADDING } from '../types/lib/size';
 
 interface IVSPContainerProps {
 	/**
@@ -30,13 +28,6 @@ interface IVSPContainerProps {
  * @property
  * - ```wrapSafeAreaView```: Wrap in SafeAreaView if true (by default ```false```)
  * - ```justifyContent```: Justify Content (by default ```flex-start```)
- * - ```padding```: Overall padding; including paddingTop, paddingBottom, paddingRight and paddingLeft
- * - ```paddingX```: Horizontal padding; including paddingRight and paddingLeft
- * - ```paddingY```: Vertical padding; including paddingTop and paddingBottom
- * - ```paddingTop```: Top padding
- * - ```paddingBottom```: Bottom padding
- * - ```paddingRight```: Rigth padding
- * - ```paddingLeft```: Left padding
  */
 export default class VSPContainer extends React.Component<
 	IVSPPaddingProps<IVSPContainerProps>
@@ -57,7 +48,6 @@ export default class VSPContainer extends React.Component<
 				flexDirection: 'column',
 				justifyContent: this.props.justifyContent!,
 				alignItems: 'stretch',
-				...decodeVSPPaddingProps(this.props),
 			},
 		});
 
