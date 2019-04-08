76bc87b85712be965b739ceca3028a149f1124dd
Sookhyun Kwon <rnjssh96@gmail.com>
2019-04-08 21:36:28 +0900

    Adjust the size

--- src/components/vsp-header-button.tsx
+++ src/components/vsp-header-button.tsx
@@ -4,7 +4,7 @@ import { NavigationScreenProp, Header } from 'react-navigation';
 
 import { RawColor, THEME_COLORS } from '../types/lib/theme';
 
-const VSP_HEADER_BUTTON_SIZE = Header.HEIGHT * 0.3;
+const VSP_HEADER_BUTTON_SIZE = Header.HEIGHT * 0.35;
 
 interface IVSPHeaderButtonProps {
 	/**
