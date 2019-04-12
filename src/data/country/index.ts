02d52c6f21f62ed95080c72edfe375c88e38804d
Sookhyun Kwon <rnjssh96@gmail.com>
2019-04-12 15:28:29 +0900

    Move Country type to country.ts

--- src/data/country/index.ts
+++ src/data/country/index.ts
@@ -1,47 +1,6 @@
 import * as _countries from './countries.json';
 
-import { CountryCode } from '../../types/data/country.js';
-
-export type Country = {
-	name: string;
-	topLevelDomain: string[];
-	alpha2Code: string;
-	alpha3Code: CountryCode;
-	callingCodes: string[];
-	capital: string;
-	altSpellings: string[];
-	region: string;
-	subregion: string;
-	population: number;
-	latlng: number[];
-	demonym: string;
-	area: number | null;
-	gini: number | null;
-	timezones: string[];
-	borders: string[];
-	nativeName: string;
-	numericCode: string | null;
-	currencies: {
-		code: string | null;
-		name: string | null;
-		symbol: string | null;
-	}[];
-	languages: {
-		iso639_1: string | null;
-		iso639_2: string;
-		name: string;
-		nativeName: string;
-	}[];
-	translations: { ko: string; [key: string]: string | null };
-	flag: string;
-	regionalBlocs: {
-		acronym: string;
-		name: string;
-		otherAcronyms: string[];
-		otherNames: string[];
-	}[];
-	cioc: string | null;
-};
+import { CountryCode, Country } from '../../types/data/country';
 
 const COUNTRIES: Country[] = _countries.countries;
 
