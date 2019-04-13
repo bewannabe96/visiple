325adf3f849c5925da6a6ce2247d7bbe8a75e341
Sookhyun Kwon <rnjssh96@gmail.com>
2019-04-14 01:53:28 +0900

    Update member names

--- src/types/data/user.ts
+++ src/types/data/user.ts
@@ -3,7 +3,7 @@ export type UserID = number;
 export type Email = string;
 
 export interface User {
-	userID: UserID;
-	userName: string;
-	userEmail: Email;
+	id: UserID;
+	name: string;
+	email: Email;
 }
