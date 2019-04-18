a0e3186bd486e66f7f0d98599a3d383678263de8
Sookhyun Kwon <rnjssh96@gmail.com>
2019-04-18 20:40:08 +0900

    Remove navigation bar

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -7,6 +7,10 @@ import VSPContainer from '../components/vsp-container';
 import ProfileScreen from './profile-screen';
 
 export default class HomeScreen extends React.Component<IVSPScreenProps> {
+	public static navigationOptions = {
+		header: null,
+	};
+
 	public render() {
 		return (
 			<VSPContainer wrapSafeAreaView={true}>
