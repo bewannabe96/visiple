3073acd20004d9ddfae3fbb9ce44736b012902a5
Sookhyun Kwon <rnjssh96@gmail.com>
2019-05-01 09:45:37 +0900

    Add LOCALIZE_TIMEZONE

--- src/types/lib/localize.ts
+++ src/types/lib/localize.ts
@@ -1,3 +1,5 @@
 import * as RNLocalize from 'react-native-localize';
 
 export const LOCALIZE_SYSTEM_LANGUAGE = RNLocalize.getLocales()[0].languageCode;
+
+export const LOCALIZE_TIMEZONE = RNLocalize.getTimeZone();
