49c7821fd67a3d3c2bc716aec31bf71cbf7292a3
Sookhyun Kwon <rnjssh96@gmail.com>
2019-05-07 17:04:29 +0900

    Encapsulate ProfileView

---
+++ src/screen-components/profile-view/index.tsx
@@ -0,0 +1,246 @@
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { FlatList, Header } from 'react-navigation';
+import { Avatar, Text, Image } from 'react-native-elements';
+
+import { THEME_COLORS } from '../../types/lib/theme';
+import {
+	HORIZONTAL_UNIT,
+	VSP_EDGE_PADDING,
+	THEME_HEADER_FONTSIZE,
+	DEVICE_WIDTH,
+} from '../../types/lib/size';
+import { TravelLog } from '../../types/data/travel-log';
+
+import VSPText from '../../components/vsp-text';
+
+const PROFILE_VIEW_HEIGHT = HORIZONTAL_UNIT(35);
+const SUMMARY_VIEW_HEIGHT = HORIZONTAL_UNIT(15);
+
+const PROFILE_VIEW_OPACITY_SPEED = 4 / 5;
+
+export interface IProfileViewProps {
+	/**
+	 * Travel logs of the user
+	 */
+	travelLogs: TravelLog[];
+}
+
+/**
+ * ProfileView
+ *
+ * @property
+ * - ```travelLogs```(required): Travel logs of the user
+ */
+export default class ProfileView extends React.Component<IProfileViewProps> {
+	public state = {
+		scrollY: new Animated.Value(0),
+	};
+
+	public render() {
+		const animatedProfileViewHeight = this.state.scrollY.interpolate({
+			inputRange: [0, PROFILE_VIEW_HEIGHT],
+			outputRange: [PROFILE_VIEW_HEIGHT, 0],
+			extrapolate: 'clamp',
+		});
+
+		const animatedProfileViewOpacity = this.state.scrollY.interpolate({
+			inputRange: [0, PROFILE_VIEW_HEIGHT * PROFILE_VIEW_OPACITY_SPEED],
+			outputRange: [1, 0],
+			extrapolate: 'clamp',
+		});
+
+		return (
+			<View>
+				<FlatList
+					data={this.props.travelLogs}
+					keyExtractor={item => item.id.toString()}
+					ListHeaderComponent={
+						<View style={style.scrollPaddingSpacer} />
+					}
+					renderItem={({ item }) => (
+						<TouchableOpacity
+							style={style.logItemContainer}
+							activeOpacity={0.6}
+							onPress={() => {}}
+						>
+							<View style={style.logImageView}>
+								<Image
+									source={require('../../dev-sample-image/landscape_1.jpeg')}
+								/>
+							</View>
+							<View style={style.logTitleView}>
+								<VSPText
+									fontSize={THEME_HEADER_FONTSIZE}
+									color={THEME_COLORS.white}
+								>
+									{item.title}
+								</VSPText>
+							</View>
+						</TouchableOpacity>
+					)}
+					scrollEventThrottle={1}
+					onScroll={Animated.event([
+						{
+							nativeEvent: {
+								contentOffset: { y: this.state.scrollY },
+							},
+						},
+					])}
+					showsVerticalScrollIndicator={false}
+				/>
+				<View style={style.collapsibleHeader}>
+					<Animated.View
+						style={[
+							style.profileView,
+							{
+								height: animatedProfileViewHeight,
+								opacity: animatedProfileViewOpacity,
+							},
+						]}
+					>
+						<Avatar size={HORIZONTAL_UNIT(20)} />
+						<Text h2 style={{ marginTop: HORIZONTAL_UNIT(2) }}>
+							홍길동
+						</Text>
+						<Text
+							h3
+							style={{
+								color: THEME_COLORS.oceanBlue,
+								marginTop: HORIZONTAL_UNIT(),
+							}}
+						>
+							testuser@gmail.com
+						</Text>
+						{/* <View style={style.profileButtonView}>
+							<Button
+								icon={{
+									name: 'add-user',
+									type: 'vspicon',
+									color: THEME_COLORS.brown,
+									size: THEME_MINOR_FONTSIZE,
+								}}
+								title='친구 추가'
+								type='outline'
+								titleStyle={{
+									fontSize: THEME_MINOR_FONTSIZE,
+									color: THEME_COLORS.brown,
+								}}
+								buttonStyle={{
+									borderColor: THEME_COLORS.brown,
+									height: HORIZONTAL_UNIT(8),
+								}}
+								containerStyle={{
+									marginRight: HORIZONTAL_UNIT(2),
+									flex: 1,
+								}}
+							/>
+							<Button
+								title='팔로우'
+								titleStyle={{
+									fontSize: THEME_MINOR_FONTSIZE,
+								}}
+								buttonStyle={{
+									backgroundColor: THEME_COLORS.brown,
+									height: HORIZONTAL_UNIT(8),
+								}}
+								containerStyle={{
+									flex: 1,
+								}}
+							/>
+						</View> */}
+					</Animated.View>
+					<View style={style.summaryView}>
+						<View style={style.summaryItemView}>
+							<VSPText style={style.summaryItemValueText}>
+								0
+							</VSPText>
+							<VSPText style={style.summaryItemTitleText}>
+								로그
+							</VSPText>
+						</View>
+						<View style={style.summaryItemView}>
+							<VSPText style={style.summaryItemValueText}>
+								0
+							</VSPText>
+							<VSPText style={style.summaryItemTitleText}>
+								친구
+							</VSPText>
+						</View>
+						<View style={style.summaryItemView}>
+							<VSPText style={style.summaryItemValueText}>
+								0
+							</VSPText>
+							<VSPText style={style.summaryItemTitleText}>
+								팔로워
+							</VSPText>
+						</View>
+					</View>
+				</View>
+			</View>
+		);
+	}
+}
+
+const style = StyleSheet.create({
+	collapsibleHeader: {
+		position: 'absolute',
+		top: 0,
+		width: '100%',
+		backgroundColor: THEME_COLORS.white,
+	},
+
+	profileView: {
+		paddingHorizontal: VSP_EDGE_PADDING,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+
+	// profileButtonView: {
+	// 	flexDirection: 'row',
+	// 	alignItems: 'center',
+	// 	marginTop: HORIZONTAL_UNIT(1.5),
+	// },
+
+	summaryView: {
+		flexDirection: 'row',
+		paddingVertical: HORIZONTAL_UNIT(4),
+		height: SUMMARY_VIEW_HEIGHT,
+	},
+
+	scrollPaddingSpacer: {
+		height: PROFILE_VIEW_HEIGHT + SUMMARY_VIEW_HEIGHT,
+	},
+
+	summaryItemView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	summaryItemValueText: {
+		color: THEME_COLORS.black,
+		marginBottom: 5,
+	},
+
+	summaryItemTitleText: {
+		fontWeight: 'bold',
+		color: THEME_COLORS.oceanBlue,
+	},
+
+	logItemContainer: {
+		backgroundColor: THEME_COLORS.black,
+	},
+
+	logImageView: {
+		opacity: 0.7,
+		height: DEVICE_WIDTH * 0.6,
+	},
+
+	logTitleView: {
+		position: 'absolute',
+		bottom: 0,
+		width: '100%',
+		padding: HORIZONTAL_UNIT(3),
+	},
+});
--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,13 +1,74 @@
 import React from 'react';
 import { NavigationScreenProp } from 'react-navigation';
+import { DateTime } from 'luxon';
 
 import { IVSPScreenProps } from '../types/props/vsp-screen';
+import { TravelLog } from '../types/data/travel-log';
 
 import VSPContainer from '../components/vsp-container';
 import VSPHeader from '../components/vsp-header';
 import VSPHeaderButton from '../components/vsp-header-button';
 
-import ProfileScreen from './profile-screen';
+import ProfileScreen from '../screen-components/profile-view';
+
+const DEV_TRAVEL_LOG: TravelLog[] = [
+	{
+		id: 1,
+		title: 'Travel Log 1',
+		titleImage: '',
+		owner: 1,
+		participants: [1, 2],
+		period: {
+			from: DateTime.local(),
+			to: DateTime.local(),
+		},
+		days: [],
+		published: false,
+		countryCodes: ['KOR'],
+	},
+	{
+		id: 2,
+		title: 'Travel Log 2',
+		titleImage: '',
+		owner: 1,
+		participants: [1, 2],
+		period: {
+			from: DateTime.local(),
+			to: DateTime.local(),
+		},
+		days: [],
+		published: false,
+		countryCodes: ['KOR'],
+	},
+	{
+		id: 3,
+		title: 'Travel Log 3',
+		titleImage: '',
+		owner: 1,
+		participants: [1, 2],
+		period: {
+			from: DateTime.local(),
+			to: DateTime.local(),
+		},
+		days: [],
+		published: false,
+		countryCodes: ['KOR'],
+	},
+	{
+		id: 4,
+		title: 'Travel Log 4',
+		titleImage: '',
+		owner: 1,
+		participants: [1, 2],
+		period: {
+			from: DateTime.local(),
+			to: DateTime.local(),
+		},
+		days: [],
+		published: false,
+		countryCodes: ['KOR'],
+	},
+];
 
 export default class HomeScreen extends React.Component<IVSPScreenProps> {
 	public static navigationOptions = ({
@@ -26,15 +87,20 @@ export default class HomeScreen extends React.Component<IVSPScreenProps> {
 							}}
 						/>
 					}
+					transparent
 				/>
 			),
 		};
 	};
 
+	public state = {
+		headerViewOpacity: undefined,
+	};
+
 	public render() {
 		return (
 			<VSPContainer wrapSafeAreaView>
-				<ProfileScreen navigation={this.props.navigation} />
+				<ProfileScreen travelLogs={DEV_TRAVEL_LOG} />
 			</VSPContainer>
 		);
 	}
