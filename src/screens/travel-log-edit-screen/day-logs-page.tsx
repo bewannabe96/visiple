032cd4118ee12ed51aee9ad7d4ce83adc25d83da
Sookhyun Kwon <rnjssh96@gmail.com>
2019-05-14 15:45:11 +0900

    Add title to header

--- src/screens/travel-log-edit-screen/day-logs-page.tsx
+++ src/screens/travel-log-edit-screen/day-logs-page.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
 import { NavigationScreenProp, withNavigation } from 'react-navigation';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 import { DateTime } from 'luxon';
 
 import { Period } from '../../types/data/datetime';
 import { IVSPScreenProps } from '../../types/props/vsp-screen';
+import { TravelLog } from '../../types/data/travel-log';
 
-import VSPHeader from '../../components/vsp-header';
+import VSPHeader, {
+	VSP_PURE_HEADER_HEIGHT,
+	VSP_STATUS_BAR_HEIGHT,
+} from '../../components/vsp-header';
 import { VSPHeaderBack } from '../../components/vsp-header-button';
 import VSPContainer from '../../components/vsp-container';
 
 import DayLogsTabTabbar from './day-logs-tab-tabbar';
 import DayLog from './day-log';
 
-const DEV_PERIOD = {
-	from: DateTime.local().startOf('day'),
-	to: DateTime.local()
-		.startOf('day')
-		.plus({ days: 6 }),
+const DEV_TRAVEL_LOG: TravelLog = {
+	id: 1,
+	title: 'Travel Log 1',
+	titleImage: '',
+	owner: 1,
+	participants: [1, 2],
+	period: {
+		from: DateTime.local().startOf('day'),
+		to: DateTime.local()
+			.startOf('day')
+			.plus({ days: 6 }),
+	},
+	published: false,
+	countryCodes: ['KOR'],
+	days: [],
 };
 
 interface IDayLogsPageProps {
+	/**
+	 * Title of the travel log
+	 */
+	title: string;
+
 	/**
 	 * Period of the DayLogsTabView
 	 */
@@ -31,6 +52,7 @@ interface IDayLogsPageProps {
  * DayLogsPage
  *
  * @property
+ * - ```title```(required): Title of the travel log
  * - ```period```(required): Period of the DayLogsTabView
  */
 class DayLogsPage extends React.Component<IVSPScreenProps<IDayLogsPageProps>> {
@@ -43,13 +65,15 @@ class DayLogsPage extends React.Component<IVSPScreenProps<IDayLogsPageProps>> {
 			header: (
 				<VSPHeader
 					headerLeft={<VSPHeaderBack navigation={navigation} />}
+					transparent
 				/>
 			),
 		};
 	};
 
 	static defaultProps = {
-		period: DEV_PERIOD,
+		title: DEV_TRAVEL_LOG.title,
+		period: DEV_TRAVEL_LOG.period,
 	};
 
 	public render() {
@@ -65,6 +89,9 @@ class DayLogsPage extends React.Component<IVSPScreenProps<IDayLogsPageProps>> {
 
 		return (
 			<VSPContainer wrapSafeAreaView>
+				<View style={style.headerView}>
+					<Text h3>{this.props.title}</Text>
+				</View>
 				<ScrollableTabView
 					renderTabBar={() => (
 						<DayLogsTabTabbar period={this.props.period} />
@@ -76,5 +103,14 @@ class DayLogsPage extends React.Component<IVSPScreenProps<IDayLogsPageProps>> {
 		);
 	}
 }
+
+const style = StyleSheet.create({
+	headerView: {
+		marginTop: VSP_STATUS_BAR_HEIGHT,
+		height: VSP_PURE_HEADER_HEIGHT,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
 
 export default withNavigation(DayLogsPage);
